4615b1157f5d520b413163424545dd3c2ed2bb61
Sirius1616 <siriusa1.615@gmail.com>
2024-10-08 22:58:22 +0000

    Data manipulate

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 function getListStudents(){
-	const arrays = new Array({ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	const arrays = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 	{ id: 2, firstName: 'James', location: 'Columbia' },
-	{ id: 5, firstName: 'Serena', location: 'San Francisco' })
+	{ id: 5, firstName: 'Serena', location: 'San Francisco' }]
 
 	return arrays
 }
